441bab62b56a567ac0b670fd976b6fa5c348898c
BOLO747 <rbolo062@uottawa.ca>
2016-01-13 12:54:28 -0500

    Update ShopifyAPIClient.rb

--- ShopifyAPIClient.rb
+++ ShopifyAPIClient.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 class ShopifyAPIClient
   
   # attr_reader :shop_id
@@ -14,26 +16,26 @@ class ShopifyAPIClient
  
   def orders
     params = authenticate
-    orders_json = some_http_library.get("https://www.shopify.this.is.a.sample.com/#{shop_id}/orders", params)
+    orders_json = HTTParty.get("https://www.shopify.this.is.a.sample.com/#{shop_id}/orders", params)
     return JSON.parse(orders_json)
   end
  
   def products
     params = authenticate
-    products_json = some_http_library.get("https://www.shopify.this.is.a.sample.com/#{shop_id}/products", params)
+    products_json = HTTParty.get("https://www.shopify.this.is.a.sample.com/#{shop_id}/products", params)
     return JSON.parse(products_json)
   end
  
   def product(id)
     params = authenticate
-    product_json = some_http_library.get("https://www.shopify.this.is.a.sample.com/#{shop_id}/products/#{id}", params)
+    product_json = HTTParty.get("https://www.shopify.this.is.a.sample.com/#{shop_id}/products/#{id}", params)
     return JSON.parse(product_json)
   end
   
   #3
   def authenticate
     params = Hash.new
-    params[:basic_auth] = {username: SECRET_API_KEY, password: ''} #username could also be wrong, im not sure
+    params[:basic_auth] = {username: ENV['api_key'], password: ''} #username and password could also be wrong, im not sure
     return params
   end
 end
